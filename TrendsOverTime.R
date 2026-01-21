2f213e536366f6bb429d17373d7ca4a77725e32f
ViktorGaardman <viktorg97@live.se>
2026-01-21 16:21:29 +0100

    Trying trait analyses

--- TrendsOverTime.R
+++ TrendsOverTime.R
@@ -107,6 +107,42 @@ df_long <- subset(df_long, !species == 0)
 df_long <- df_long %>% 
   mutate(across(c("Fire_Int_Groups", "Continent", "StudyID.x"), as.factor))
 
+#Create a plantgroup column 
+df_long <- df_long %>%
+  mutate(
+    PlantGroup = str_extract(base, "(?<=_)[^_]+(?=_)")
+  )
+
+#Filter out bryophytes
+df_traits <- df_long %>%
+  filter(! PlantGroup %in% "bryophyte")
+
+#Look at the data
+ggplot(data = df_traits, aes(x = log(Seed_dry_mass))) +
+  geom_histogram() #Log
+
+ggplot(data = df_traits, aes(x = log(Plant_height_vegetative))) +
+  geom_histogram() #Log
+
+ggplot(data = df_traits, aes(x = log(Leaf_nitrogen))) +
+  geom_histogram() #log
+
+ggplot(data = df_traits, aes(x = log(Leaf_area_PI))) +
+  geom_histogram() #log
+
+ggplot(data = df_traits, aes(x = log(Seed_longevity+0.01))) +
+  geom_histogram() 
+
+#Decide on leaf area PE vs PI and vegetative vs generative plant height
+
+#PI has more data than PE
+#vegetative has more data than generative
+
+#Dispersal unit dry mass has 474 observations (1326 missing). Skip
+#this trait
+#Seedlongevity has 1119 observations (681 missing). We can try it
+#but also much data lacking.
+
 #Question: What is the relative dominance
 #of each trait across time, continent, and fire intensity. Is it affected
 #by mean temp, perc, or soil water content?
@@ -123,14 +159,71 @@ missing_species <- setdiff(species_try, species_df)
 
 missing_species
 
-Seed_mass_mod <- lmer(log(Seed_dry_mass) ~
-                        Years_since_fire +
-                        Avg_Temp+
-                        AvgPer+
-                        Fire_Int_Groups +
-                        Latitude*Continent +
-                        (1|RowID/StudyID.x),
-                      data = df_long) 
+#Scale and center percipitation and temperature
+df_traits$Temp_sc <- as.numeric(scale(df_traits$Avg_Temp, center = TRUE, scale = TRUE))
+df_traits$Per_sc <- as.numeric(scale(df_traits$AvgPer, center = TRUE, scale = TRUE))
+
+#Add average genera level trait values to species_sp
+#1. Define traits
+traits <- c(
+  "Seed_dry_mass",
+  "Seed_longevity",
+  "Plant_height_vegetative",
+  "Leaf_nitrogen",
+  "Leaf_area_PI"
+)
+
+
+#2. Define genus/species info
+df_traits <- df_traits %>%
+  mutate(
+    genus = str_extract(species, "^[^_]+"),
+    species2 = str_extract(species, "(?<=_).+"),
+    is_genus_level = species2 %in% c("sp.", "sp")
+  )
+
+#3. Calculate genus level mean trait values for all genera
+#that has data for more than 1 species
+genus_traits <- df_traits %>%
+  filter(!is_genus_level) %>%
+  group_by(genus) %>%
+  summarise(
+    across(
+      all_of(traits),
+      ~ mean(.x, na.rm = TRUE),
+      .names = "{.col}_genus_mean"
+    ),
+    n_species = n_distinct(species2),
+    .groups = "drop"
+  ) %>%
+  filter(n_species >= 2)
+
+
+#4. Add to dataset
+df_filled <- df_traits %>%
+  left_join(genus_traits, by = "genus") %>%
+  mutate(
+    across(
+      all_of(traits),
+      ~ if_else(
+        is_genus_level & is.na(.x),
+        get(paste0(cur_column(), "_genus_mean")),
+        .x
+      )
+    )
+  ) %>%
+  select(-ends_with("_genus_mean"))
+
+df_filled$species <- as.factor(df_filled$species)
+
+Seed_mass_mod <- lm(log(Seed_dry_mass) ~
+                        Years_since_fire*Fire_Int_Groups +
+                        Continent +
+                        Per_sc +
+                        Temp_sc,
+                      weights = log(cover+1),
+                      data = df_filled) 
+
 
 car::Anova(Seed_mass_mod, type ='III')
 
@@ -141,19 +234,120 @@ check_outliers(Seed_mass_mod)
 sim_res <- simulateResiduals(fittedModel = Seed_mass_mod, n = 500)
 plot(sim_res)
 
-ggplot(df_long, aes(x=Years_since_fire, y = log(Seed_dry_mass))) +
-  geom_point(aes(color = Fire_Int_Groups))+
-  stat_smooth(method='lm', aes(color = Fire_Int_Groups))
+#Plot predictions!
+pred_grid <- expand.grid(
+  Years_since_fire = seq(
+    min(df_filled$Years_since_fire, na.rm = TRUE),
+    max(df_filled$Years_since_fire, na.rm = TRUE),
+    length.out = 88
+  ),
+  Fire_Int_Groups = levels(df_filled$Fire_Int_Groups),
+  Continent     = levels(df_filled$Continent)
+) %>%
+  mutate(
+    Temp_sc   = mean(df_filled$Temp_sc, na.rm = TRUE),
+    Per_sc = mean(df_filled$Per_sc, na.rm = TRUE),
+    cover     = mean(df_filled$cover, na.rm = TRUE)
+  )
 
-Plant_height_mod <- lmer(Plant_height_vegetative ~
-                           Years_since_fire +
-                           Fire_Int_Groups +
-                           Continent +
-                           SWI +
-                           Avg_Temp +
-                           AvgPer +
-                           (1|StudyID.x),
-                         data = df_long) 
+
+pred <- predict(
+  Seed_mass_mod,
+  newdata = pred_grid,
+  type = "response",      
+  se.fit = TRUE,
+  re.form = NA
+)
+
+eta <- pred$fit
+se  <- pred$se.fit
+
+lower_eta <- eta - 1.96 * se
+upper_eta <- eta + 1.96 * se
+
+# inverse link = plogis for beta regression
+pred_grid$fit   <- plogis(eta)
+pred_grid$lower <- plogis(lower_eta)
+pred_grid$upper <- plogis(upper_eta)
+
+
+pred_grid$Fire_Int_Groups <- factor(
+  pred_grid$Fire_Int_Groups,
+  levels = c("High", "Medium", "Low")
+)
+
+seedmassplot<- ggplot(pred_grid,
+                      aes(x = Years_since_fire,
+                          y = fit,
+                          color = Fire_Int_Groups)) +
+  geom_line(linewidth = 1.2) +
+  facet_wrap(~ Continent,
+             labeller = labeller(
+               Continent = c(
+                 "Eurasia" = "Eurasia",
+                 "North_America" = "North America"))) +
+  geom_ribbon(aes(ymin = lower, ymax = upper, fill = Fire_Int_Groups),
+              alpha = 0.2, color = NA, show.legend = FALSE) +
+  scale_color_manual(values = c("firebrick", "goldenrod", "cornflowerblue")) + 
+  scale_fill_manual(values = c("firebrick", "goldenrod", "cornflowerblue")) + 
+  labs(
+    x = "Time since fire (years)",
+    y = "Predicted value",
+    color = "Fire intensity"
+  ) +
+  theme_bw() +
+  ggtitle("Seed dry mass")+
+  theme(legend.position="none",
+        legend.text=element_text(size=16),
+        legend.title=element_text(size=18),
+        legend.direction='vertical',
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=18),
+        axis.text = element_text(size = 14),
+        strip.text = element_text(size=16),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank(),
+        plot.title = element_text(size = 18, hjust = 0.5)) 
+
+
+seedmassplot
+
+ggsave(plot = seedmassplot, filename = "Seedmass_plot.png", dpi =300,
+       height = 4.2, width = 6.5)
+
+temppred <- ggpredict(Seed_mass_mod, terms = "Temp_sc")
+
+#Do we need to predict for temp and precipitation? I think not.
+#One example here kept in case we want it later.
+plottemp <- ggplot(temppred, aes(x = x, y = predicted)) +
+  geom_line(linewidth = 0.8, color = "firebrick") +
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high),
+              fill = "red", alpha = 0.3) +
+  theme_minimal() +
+  ylab("Predicted value") +
+  xlab("Temperature (scaled)") +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    title = element_text(size = 14),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.line = element_line(color = "black")
+  )
+
+plottemp
+
+
+##PLANT HEIGHT MODEL
+
+Plant_height_mod <- lm(log(Plant_height_vegetative) ~
+                             Years_since_fire +
+                         Fire_Int_Groups +
+                             Continent +
+                         Temp_sc +
+                         Per_sc,
+                           weights = log(cover+1),
+                           data = df_filled) 
 
 car::Anova(Plant_height_mod, type ='II')
 
@@ -164,26 +358,67 @@ check_outliers(Plant_height_mod)
 sim_res <- simulateResiduals(fittedModel = Plant_height_mod, n = 500)
 plot(sim_res)
 
-ggplot(df_long, aes(x=Years_since_fire, y = log(Seed_dry_mass))) +
-  geom_point(aes(color = Fire_Int_Groups))+
-  stat_smooth(method='lm', aes(color = Fire_Int_Groups))+
-  facet_wrap(~Continent)
+#Leaf nitrogen
+
+leaf_nig_mod <- lm(log(Leaf_nitrogen) ~
+        Years_since_fire *
+        Fire_Int_Groups +
+          Years_since_fire * Temp_sc +
+          Temp_sc +
+          Per_sc,
+      weights = log(cover+1),
+      data = df_filled) 
+
+car::Anova(leaf_nig_mod, type ='II')
+
+check_normality(leaf_nig_mod)  # Tests if residuals are normally distributed
+check_heteroscedasticity(leaf_nig_mod)  # Checks if residual variance is consistent
+check_collinearity(leaf_nig_mod)
+check_outliers(leaf_nig_mod)
+sim_res <- simulateResiduals(fittedModel = leaf_nig_mod, n = 500)
+plot(sim_res)
 
 
+#Leaf_area
 
+leaf_area_mod <- lm(log(Leaf_area_PI) ~
+                     Years_since_fire +
+                     Fire_Int_Groups +
+                     Continent +
+                     Temp_sc +
+                     Per_sc,
+                   weights = log(cover+1),
+                   data = df_filled) 
 
+car::Anova(leaf_nig_mod, type ='II')
+
+check_normality(leaf_nig_mod)  # Tests if residuals are normally distributed
+check_heteroscedasticity(leaf_nig_mod)  # Checks if residual variance is consistent
+check_collinearity(leaf_nig_mod)
+check_outliers(leaf_nig_mod)
+sim_res <- simulateResiduals(fittedModel = leaf_area_mod, n = 500)
+plot(sim_res)
+
+#Seed longevity
+seed_long_mod <- lm(Seed_longevity ~
+                      Years_since_fire +
+                      Fire_Int_Groups +
+                      Continent +
+                      Temp_sc +
+                      Per_sc,
+                    data = df_filled) 
+
+car::Anova(leaf_nig_mod, type ='II')
+
+check_normality(leaf_nig_mod)  # Tests if residuals are normally distributed
+check_heteroscedasticity(leaf_nig_mod)  # Checks if residual variance is consistent
+check_collinearity(leaf_nig_mod)
+check_outliers(leaf_nig_mod)
+sim_res <- simulateResiduals(fittedModel = leaf_area_mod, n = 500)
+plot(sim_res)
 
 
 ##################
-#Community cover tests using a beta-regression
-#1. Find a new way to describe three species
-#2. Are any effects non-linear?
-
-#Create a plantgroup column 
-df_long <- df_long %>%
-  mutate(
-    PlantGroup = str_extract(base, "(?<=_)[^_]+(?=_)")
-  )
 
 #Create a total size of study column
 df_long <- df_long %>%
@@ -196,11 +431,6 @@ df_long <- df_long %>%
 #avoid 0 and 1 one in the dataset (beta must be >0 and <1)
 
 df_long$coverstd <- (df_long$cover + 0.01) / 101
-
-#Scale and center Latitude, percipitation, and temperature
-df_long$Temp_sc <- scale(df_long$Avg_Temp, center = TRUE, scale = TRUE)
-df_long$Per_sc <- scale(df_long$AvgPer, center = TRUE, scale = TRUE)
-df_long$Latitude_sc <- scale(df_long$Latitude, center = TRUE, scale = TRUE)
 
 df_long$base <- as.factor(df_long$base)
 
